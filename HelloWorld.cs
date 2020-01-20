71e1881cd9d10c04a41c9cd948cfb82201d3bc17
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-01-20 23:08:51 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+			//declaration of string variables
+			string text="Hello";
+			string text1=" World";
+			string text2="!";
+			//printing the output to the screen
+			Console.WriteLine(text+text1+text2);
+			//wait till key pressed to exit
+			Console.ReadKey();
+        }
+    }
+}
